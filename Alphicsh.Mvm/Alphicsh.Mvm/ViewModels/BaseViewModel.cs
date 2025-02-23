07f90da305e59d01d9563416ece29340ceca1cb4
Alice Jankowska <alphish.creature@gmail.com>
2025-02-23 15:38:37 +0100

    ISS-1: Implemented a base view model with methods to create relay properties and disposing them on own disposal

---
+++ Alphicsh.Mvm/Alphicsh.Mvm/ViewModels/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Alphicsh.Mvm.Sources;
+
+namespace Alphicsh.Mvm.ViewModels;
+
+public class BaseViewModel : IViewModel
+{
+    protected ICollection<IViewModelProperty> OwnProperties { get; } = new List<IViewModelProperty>();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected TProperty RegisterProperty<TProperty>(TProperty property)
+        where TProperty : IViewModelProperty
+    {
+        OwnProperties.Add(property);
+        return property;
+    }
+
+    protected RelayViewModelProperty<TValue> RelayProperty<TValue>(string propertyName, IValueSource<TValue> valueSource)
+        => RegisterProperty(new RelayViewModelProperty<TValue>(this, propertyName, valueSource));
+
+    public void RaisePropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public void Dispose()
+    {
+        foreach (var property in OwnProperties)
+            property.Dispose();
+    }
+}

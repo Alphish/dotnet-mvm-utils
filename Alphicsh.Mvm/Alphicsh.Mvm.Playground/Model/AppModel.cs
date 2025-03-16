99b50c0547de1b136e249345c806abf18a595569
Alice Jankowska <alphish.creature@gmail.com>
2025-03-16 22:26:55 +0100

    ISS-2: Added a basic predicate command to the playground application

--- Alphicsh.Mvm/Alphicsh.Mvm.Playground/Model/AppModel.cs
+++ Alphicsh.Mvm/Alphicsh.Mvm.Playground/Model/AppModel.cs
@@ -7,8 +7,17 @@ public class AppModel
     public ValueSource<string> TextSource { get; set; }
     public string Text { get => TextSource.Value; set => TextSource.Value = value; }
 
+    public ValueSource<bool> UppercaseAvailableSource { get; set; }
+    public bool UppercaseAvailable { get => UppercaseAvailableSource.Value; set => UppercaseAvailableSource.Value = value; }
+
     public AppModel()
     {
         TextSource = ValueSource.Create("Hello!");
+        UppercaseAvailableSource = ValueSource.Create(false);
+    }
+
+    public void UppercaseText()
+    {
+        Text = Text.ToUpperInvariant();
     }
 }
--- Alphicsh.Mvm/Alphicsh.Mvm.Playground/ViewModel/TopViewModel.cs
+++ Alphicsh.Mvm/Alphicsh.Mvm.Playground/ViewModel/TopViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Alphicsh.Mvm.Playground.Model;
+﻿using Alphicsh.Mvm.Commands;
+using Alphicsh.Mvm.Playground.Model;
 using Alphicsh.Mvm.ViewModels;
 
 namespace Alphicsh.Mvm.Playground.ViewModel;
@@ -8,8 +9,13 @@ public class TopViewModel : BaseViewModel
     public TopViewModel(AppModel model)
     {
         TopTextProperty = RelayProperty(nameof(TopText), model.TextSource);
+        UppercaseText = Command.From(() => model.UppercaseAvailable, model.UppercaseText);
+
+        model.UppercaseAvailableSource.ValueChanged += (sender, e) => UppercaseText.RaiseCanExecuteChanged();
     }
 
     public RelayViewModelProperty<string> TopTextProperty { get; }
     public string TopText { get => TopTextProperty.Value; set => TopTextProperty.Value = value; }
+
+    public IConditionalCommand UppercaseText { get; }
 }
